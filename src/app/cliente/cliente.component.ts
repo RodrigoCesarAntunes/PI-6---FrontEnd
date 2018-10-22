9949f050c785e407dc931c069d192da072c7a7bd
f55FernandaBarbiero <fernanda_estela@live.com>
2018-10-22 17:17:26 -0200

    Initial commit

---
+++ src/app/cliente/cliente.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+//import { Cliente } from '../modelo/cliente';
+
+@Component({
+  selector: 'app-cliente',
+  templateUrl: './cliente.component.html',
+  styleUrls: ['./cliente.component.css']
+})
+export class ClienteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
