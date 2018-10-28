3a8712f1031f5078d085a86622cfdbbabb59d410
f55FernandaBarbiero <fernanda_estela@live.com>
2018-10-28 19:26:47 -0200

    atualizacao barra de menu superior
    login, lista de desejos, etc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'PI5WEB LIVROS';
+  title = 'BIBLIOTECA DO ESTUDANTE';
 }
 
